7e18432e678a7664a66aefd3605be049df6e62ff
itany <itany@itany.com>
2017-09-18 17:07:36 +0800

    ddd

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -5,11 +5,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent  {
 
-  constructor() { }
+  books:Array<any> = [
+    {
+      "title":'bookTiele-1',
+      "price":300+parseInt(Math.random()*10+""),
+      "content":"this is a very good ",
+      "star":4
+    },
+    {
+      "title":'bookTiele-2',
+      "price":300+parseInt(Math.random()*10+""),
+      "content":"this is a very good ",
+      "star":5
+    },
+    {
+      "title":'bookTiele-3',
+      "price":300+parseInt(Math.random()*10+""),
+      "content":"this is a very good ",
+      "star":2
+    },
+  ];
 
-  ngOnInit() {
-  }
+
 
 }
